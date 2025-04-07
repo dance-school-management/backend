b66d97bd5f78d1eef696ebd6c341a3b8f38a92fb
Mateusz Wala <mateusz.wala2734@gmail.com>
2025-04-07 20:33:26 +0000

    feat: Add first version of event calendar

--- packages/ui/src/components/calendar.tsx
+++ packages/ui/src/components/calendar.tsx
@@ -5,7 +5,7 @@ import { ChevronLeft, ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
 
 import { cn } from "@repo/ui/lib/utils";
-import { buttonVariants } from "@repo/ui/components/button";
+import { buttonVariants } from "@repo/ui/button";
 
 function Calendar({
   className,
