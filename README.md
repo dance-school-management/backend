d6d35099c2f1743ffb27d923fd32571b39f7f6b6
Piotr255 <146641708+Piotr255@users.noreply.github.com>
2025-05-05 22:59:20 +0200

    basic docker files with npm (pnpm abandoned). Needed improvements.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # README
+
+# INFO
+- you need "db" folder with files "auth-db-password.txt" and "product-db-password.txt"
+- every microservice need env.development
--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import "dotenv/config";
 import cors from "cors";
@@ -11,22 +11,6 @@ const PRODUCT_MICROSERVICE_URL = process.env.PRODUCT_MICROSERVICE_URL;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 const AUTH_MICROSERVICE_URL = process.env.AUTH_MICROSERVICE_URL;
 
-const proxyMiddlewareProduct = createProxyMiddleware<Request, Response>({
-  target: PRODUCT_MICROSERVICE_URL,
-  changeOrigin: true,
-  pathRewrite: {
-    "^/product": "",
-  },
-});
-
-const proxyMiddlewareAuth = createProxyMiddleware<Request, Response>({
-  target: AUTH_MICROSERVICE_URL,
-  changeOrigin: true,
-  pathRewrite: {
-    "^/auth": "",
-  },
-});
-
 app.use(
   cors({
     origin: FRONTEND_URL,
@@ -38,10 +22,28 @@ app.use(cookieParser());
 
 //app.get("/test", authenticate("student"));
 
-app.use("/auth", proxyMiddlewareAuth);
+if (AUTH_MICROSERVICE_URL) {
+  const proxyMiddlewareAuth = createProxyMiddleware<Request, Response>({
+    target: AUTH_MICROSERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: {
+      "^/auth": "",
+    },
+  });
+  app.use("/auth", proxyMiddlewareAuth);
+}
 
-app.use("/product", proxyMiddlewareProduct);
-
+if (PRODUCT_MICROSERVICE_URL) {
+  console.log(PRODUCT_MICROSERVICE_URL)
+  const proxyMiddlewareProduct = createProxyMiddleware<Request, Response>({
+    target: PRODUCT_MICROSERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: {
+      "^/product": "",
+    },
+  });
+  app.use("/product", proxyMiddlewareProduct);
+}
 app.get("/", (req: Request, res) => {
   res.send("Hello from api-gateway1");
 });
