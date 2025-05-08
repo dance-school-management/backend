54ce47b9529b03387bee8d48d3560c5dbe83601a
Piotr255 <146641708+Piotr255@users.noreply.github.com>
2025-05-09 01:51:34 +0200

    Change errorHandler for more consistent error message format (for now only in product-microservice)

--- product-microservice/migrate.sh
+++ product-microservice/migrate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://postgres:postgres@localhost:5433/products"
+export DATABASE_URL="postgresql://postgres:postgres@localhost:5434/products"
 npx prisma migrate dev --name init
