5cfad1fb9bae7984b2e1f5c60f0bb6d4075c091b
Dariusz Cebula <darko2448@gmail.com>
2025-05-14 23:30:11 +0200

    Docker + migration prisma updates

--- migrate-all-prismas.sh
+++ migrate-all-prismas.sh
@@ -2,3 +2,4 @@
 (cd product-microservice && bash migrate.sh)
 (cd auth-microservice && bash migrate.sh)
 (cd enroll-microservice && bash migrate.sh)
+(cd notification-microservice && bash migrate.sh)
--- notification-microservice/migrate.sh
+++ notification-microservice/migrate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://postgres:postgres@localhost:5435/notification"
+export DATABASE_URL="postgresql://postgres:postgres@localhost:5436/notification"
 npx prisma migrate dev --name init
