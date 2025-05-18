5d5fc272dd822ef023ddf30bb51cd0a95a7ba4ad
Dariusz Cebula <darko2448@gmail.com>
2025-05-18 02:13:42 +0200

    Port changes

--- notification-microservice/migrate.sh
+++ notification-microservice/migrate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://postgres:postgres@localhost:5436/notification"
+export DATABASE_URL="postgresql://postgres:postgres@localhost:5437/notification"
 npx prisma migrate dev --name init
