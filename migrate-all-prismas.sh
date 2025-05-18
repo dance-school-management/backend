cb17a812af4ccfb3e6b84359abbd5468637d783c
Dariusz Cebula <darko2448@gmail.com>
2025-05-18 03:38:16 +0200

    Docker + migration prisma updates

--- migrate-all-prismas.sh
+++ migrate-all-prismas.sh
@@ -3,3 +3,4 @@
 (cd auth-microservice && bash migrate.sh)
 (cd enroll-microservice && bash migrate.sh)
 (cd profile-microservice && bash migrate.sh)
+(cd review-microservice && bash migrate.sh)
--- review-microservice/migrate.sh
+++ review-microservice/migrate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://postgres:postgres@localhost:5434/products"
+export DATABASE_URL="postgresql://postgres:postgres@localhost:5439/review"
 npx prisma migrate dev --name init
