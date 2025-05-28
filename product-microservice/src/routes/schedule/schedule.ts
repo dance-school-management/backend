030a0098a7ec054b898ae4c8f78424253d3c2427
Piotr255 <146641708+Piotr255@users.noreply.github.com>
2025-05-28 22:53:43 +0200

    Split schedule controller

--- product-microservice/src/routes/schedule/schedule.ts
+++ product-microservice/src/routes/schedule/schedule.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getSchedule } from "../../controllers/schedule";
+import { getSchedule, getSchedulePersonal } from "../../controllers/schedule";
 
 const router = Router();
 
 router.get("/", getSchedule);
+router.get("/personal", getSchedulePersonal);
 
 export default router;
--- product-microservice/src/routes/schedule/scheduleSwagger.ts
+++ product-microservice/src/routes/schedule/scheduleSwagger.ts
@@ -4,8 +4,7 @@
  *   get:
  *     summary: Get schedule for a given date range
  *     tags:
- *       - student
- *         - schedule
+ *       - schedule
  *     parameters:
  *       - in: query
  *         name: dateFrom
@@ -21,13 +20,34 @@
  *           type: string
  *           format: date-time
  *         example: "2025-05-31T23:59:59.000Z"
+ *     responses:
+ *       200:
+ *         description: Successful response
+ */
+
+/**
+ * @swagger
+ * /schedule/personal:
+ *   get:
+ *     summary: Get schedule for a given date range
+ *     tags:
+ *       - schedule
+ *        - personal
+ *     parameters:
  *       - in: query
- *         name: mySchedule
+ *         name: dateFrom
  *         required: true
  *         schema:
- *           type: boolean
- *         description: Show my schedule or all classes
- *         example: false
+ *           type: string
+ *           format: date-time
+ *         example: "2025-05-01T00:00:00.000Z"
+ *       - in: query
+ *         name: dateTo
+ *         required: true
+ *         schema:
+ *           type: string
+ *           format: date-time
+ *         example: "2025-05-31T23:59:59.000Z"
  *     responses:
  *       200:
  *         description: Successful response
