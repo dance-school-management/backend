37401fd507a6160571160439306b974cf6e1aa1b
Mateusz Wala <mateusz.wala2734@gmail.com>
2025-11-06 19:18:37 +0100

    chore: Add docker compose and bash scripts

--- seed-databases.sh
+++ seed-databases.sh
@@ -5,5 +5,6 @@ sleep 8
 (cd profile-microservice && bash seed.sh)
 (cd product-microservice && bash seed.sh)
 (cd enroll-microservice && bash seed.sh)
+(cd blog-microservice && bash seed.sh)
 (cd elasticsearch-microservice && (. .venv/bin/activate || source .venv/Scripts/activate) &&  bash seed.sh)
 # (cd notification-microservice && bash clear.sh)
