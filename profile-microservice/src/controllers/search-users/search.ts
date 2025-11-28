0a7b6bffff9e476b726aa750b0ec66ec92fd0cc6
Mateusz Wala <mateusz.wala2734@gmail.com>
2025-11-28 23:07:31 +0100

    chore: Construct the query in different way

--- profile-microservice/src/controllers/search-users/search.ts
+++ profile-microservice/src/controllers/search-users/search.ts
@@ -1,40 +1,36 @@
 import { Request, Response } from "express";
-import prisma from "../../utils/prisma";
 import { StatusCodes } from "http-status-codes";
-import { Role } from "../../../generated/client";
+import { Prisma, Role } from "../../../generated/client";
+import prisma from "../../utils/prisma";
 
 export async function getSearchUsers(
   req: Request<
     {},
     {},
     {},
-    {
-      query?: string;
-    }
+    { q?: string; }
   > & { user?: any; },
   res: Response,
 ) {
-  const { query } = req.query;
-  const orConditions = [];
+  const { q } = req.query;
 
-  if (query) {
-    orConditions.push({
-      OR: [
-        { name: { contains: query, mode: "insensitive" as const } },
-        { surname: { contains: query, mode: "insensitive" as const } },
-        { email: { contains: query, mode: "insensitive" as const } },
-        { phone: { contains: query, mode: "insensitive" as const } },
-      ],
-    });
+  const whereClause: Prisma.ProfileWhereInput = {
+    role: {
+      in: [Role.STUDENT]
+    }
+  };
+
+  if (q) {
+    whereClause.OR = [
+      { name: { contains: q, mode: "insensitive" as const } },
+      { surname: { contains: q, mode: "insensitive" as const } },
+      { email: { contains: q, mode: "insensitive" as const } },
+      { phone: { contains: q, mode: "insensitive" as const } },
+    ];
   }
 
   const foundUsers = await prisma.profile.findMany({
-    where: {
-      OR: orConditions,
-      role: {
-        in: [Role.STUDENT]
-      }
-    },
+    where: whereClause,
     take: 5,
   });
 
