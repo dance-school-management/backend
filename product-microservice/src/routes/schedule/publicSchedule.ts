0b4b6c3e96e3c2331f051348747b112c616a718f
Mateusz Wala <mateusz.wala2734@gmail.com>
2025-12-08 22:17:20 +0100

    feat: Add public class details endpoint (for purchase summary page)

--- product-microservice/src/routes/schedule/publicSchedule.ts
+++ product-microservice/src/routes/schedule/publicSchedule.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getCoursesClasses, getSchedule, getSearchAndFilterCourses } from "../../controllers/schedule";
+import { getClassDetails, getCoursesClasses, getSchedule, getSearchAndFilterCourses } from "../../controllers/schedule";
 
 const router = Router();
 
 router.get("/", getSchedule);
 router.get("/search/courses", getSearchAndFilterCourses);
 router.get("/courses/classes", getCoursesClasses);
+router.get("/class/:id", getClassDetails);
 
 export default router;
